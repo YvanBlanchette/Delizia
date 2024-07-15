a58cfdae3d737252c9a1f4144d43fe5965805edc
YvanBlanchette <yvanblanchette@gmail.com>
2024-07-15 19:14:30 -0400

    fix

--- service-worker.js
+++ service-worker.js
@@ -4,15 +4,20 @@ const CACHE_NAME = 'static-cache-v1';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
+    'offline.html',
 ];
-
 
 self.addEventListener('install', (evt) => {
     console.log('[ServiceWorker] Install');
     // Precache static resources here.
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
     self.skipWaiting();
 });
-
 
 self.addEventListener('activate', (evt) => {
     console.log('[ServiceWorker] Activate');
